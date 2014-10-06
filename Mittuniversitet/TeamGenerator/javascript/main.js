57c10ee38a2657f9d4e598464d03749f98797049
Max Åberg <aaberg.max@gmail.com>
2014-10-06 05:45:51 +0200

    Changes in javascript

--- Mittuniversitet/TeamGenerator/javascript/main.js
+++ Mittuniversitet/TeamGenerator/javascript/main.js
@@ -1,18 +1,18 @@
 /*  Max Åberg
  mabe1411
  aaberg.max@gmail.com   */
-var nbrOfTeams;
 var ok;
 
 function init() {
 	while (!ok) {
 		hide();
-		nbrOfTeams = prompt("Please specify the number of teams that will participate:");
+		var nbrOfTeams = prompt("Please specify the number of teams that will participate:");
 		var pattern = /^[2-9]+$|[1-9][0-9]+$/;
 		if (!pattern.test(nbrOfTeams)) {
 			window.alert("Sorry, the input can only be numbers and at least 2");
 		} else {
 			ok = true;
+			localStorage.setItem('nbrOfTeams', nbrOfTeams);
 		}
 	}
 }
@@ -32,19 +32,53 @@ function generate() {
 	text = text.replace(/\+/g, ' ');
 	var choise = text.substr(text.search("radio") + 6, 6);
 	if (choise == "random") {
-		text = text.substring(text.search("area") + 5, text.length);
-		var names = text.split("\\n");
-		document.getElementById("result").innerHTML=names[0];
+		var names = text.substring(text.search("area") + 5, text.length).split("\\n");
+		var test = new String();
+		names = names.filter(function(e) {/*Removes white-space elements*/
+			return /\S+/.test(e);
+		});
 		random(names);
 	} else {
-		var men = text.substring(text.search("mentext") + 8, text.search("womentext") - 1);
-		var women = text.substring(text.search("womentext") + 10, text.search("area") - 1);
+		var men = text.substring(text.search("mentext") + 8, text.search("womentext") - 1).split("\\n");
+		var women = text.substring(text.search("womentext") + 10, text.search("area") - 1).split("\\n");
+		men = men.filter(function(e) {/*Removes white-space elements*/
+			return /\S+/.test(e);
+		});
+		women = women.filter(function(e) {/*Removes white-space elements*/
+			return /\S+/.test(e);
+		});
 		gender(men, women);
 	}
 }
 
-function random(text) {
-
+function random(names) {
+	var nbrOfTeams = localStorage.getItem('nbrOfTeams');
+	var rest = names.length % nbrOfTeams;
+	var minimumParticipants = Math.floor(names.length / nbrOfTeams);
+	document.getElementById("result").innerHTML = names.length;
+	var distribution = "";
+	var index = 0;
+	switch(true) {
+	case (rest==1):
+		for ( i = 0; i < nbrOfTeams; i++) {
+			distribution += ("Team" + (i + 1) + "<br>");
+			for ( j = 0; j < rest + minimumParticipants; j++) {
+				index = Math.random((Math.floor * names.length));
+				/*wrong*/
+				distribution += names[index] + "<br>";
+				names.splice(index, 1);
+			}
+			distribution += "<br><br>";
+			rest = 0;
+		}
+		break;
+	case (rest>1):
+		distribution += rest;
+		break;
+	default:
+		distribution += "normal";
+	}
+	/*document.getElementById("result").innerHTML = distribution;*/
 }
 
 function gender(men, women) {
